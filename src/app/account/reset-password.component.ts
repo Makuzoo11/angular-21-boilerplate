c11cd32ec091d0045f2faefa650bc76e86c5cbc0
Mark <you@example.com>
2026-05-17 17:28:46 +0800

    fixing reset password independencies

--- src/app/account/reset-password.component.ts
+++ src/app/account/reset-password.component.ts
@@ -20,6 +20,9 @@ export class ResetPasswordComponent implements OnInit {
     form!: FormGroup;
     loading = false;
     submitted = false;
+    isValidating = true;
+    isValid = false;
+    isInvalid = false;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -50,18 +53,20 @@ export class ResetPasswordComponent implements OnInit {
         this.accountService.validateResetToken(token)
             .pipe(first())
             .subscribe({
-                next: () => {
-                    this.ngZone.run(() => {
-                        this.token = token;
-                        this.tokenStatus = TokenStatus.Valid;
-                        console.log('TOKEN STATUS SET TO VALID');
-                    });
-                },
-                error: () => {
-                    this.ngZone.run(() => {
-                        this.tokenStatus = TokenStatus.Invalid;
-                    });
-                }
+  next: () => {
+    this.ngZone.run(() => {
+        this.token = token;
+        this.isValidating = false;
+        this.isValid = true;
+        console.log('IS VALID:', this.isValid);
+    });
+},
+error: () => {
+    this.ngZone.run(() => {
+        this.isValidating = false;
+        this.isInvalid = true;
+    });
+}
             });
     }
 
